6d782a17a8eb4d86fc1e5b8d75101825a3f5aa2e
Carl <carl.chester.lloyd@gmail.com>
2020-04-15 21:31:47 -0400

    dexlist and items list with individual select

--- src/DexList.js
+++ src/DexList.js
@@ -4,6 +4,9 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import axios from 'axios';
+import { Row, Col } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 class DexList extends React.Component{
     constructor(props) {
@@ -15,8 +18,16 @@ class DexList extends React.Component{
                 { headerName: 'Name', field: 'name' },
                 { headerName: 'Number', field: 'id' }
             ],
-            rowData: [],
-            p: []
+            pokemonData: [],
+            itemData: [],
+            p: [],
+            displayMenu: true,
+            displayDexList: false,
+            displayItems: false,
+            displayPokemon: false,
+            pokemon: {},
+            displayItem: false,
+            item: {}
         };
     }
 
@@ -32,7 +43,22 @@ class DexList extends React.Component{
                 po => {
                     let p = [];
                     po.forEach((v, i) => { p.push(v.data); });
-                    this.setState({ rowData: p });
+                    this.setState({ pokemonData: p });
+                }
+        );
+
+        axios.get('https://pokeapi.co/api/v2/item/?limit=954')
+            .then(result => { return result.data.results; })
+            .then(
+                items => {
+                    const p = Promise.all(items.map(x => axios.get(`https://pokeapi.co/api/v2/item/${x.name}`)));
+                    return p;
+                })
+            .then(
+                po => {
+                    let p = [];
+                    po.forEach((v, i) => { p.push(v.data); });
+                    this.setState({ itemData: p });
                 }
             );
     }
@@ -46,17 +72,111 @@ class DexList extends React.Component{
         }
     }
 
+    getPokemon(name)
+    {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+            .then(result => { return result.data; })
+            .then(
+                po => {
+                    this.setState({ pokemon: po });
+                    this.setState({ displayDexList: false });
+                    this.setState({ displayPokemon: true });
+                }
+            );
+    }
+
+    getItem(name) {
+        axios.get(`https://pokeapi.co/api/v2/item/${name}`)
+            .then(result => { return result.data; })
+            .then(
+                i => {
+                    this.setState({ item: i });
+                    this.setState({ displayItems: false });
+                    this.setState({ displayItem: true });
+                }
+            );
+    }
+
     render(){
         return (
-            <div className="ag-theme-balham" style={{ height: '500px', width: '600px' }}>
-                <AgGridReact
-                    rowHeight={125}
-                    enableSorting={true}
-                    enableFilter={true}
-                    pagination={true}
-                    columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}>
-                </AgGridReact>
+            <div>
+                {
+                    this.state.displayMenu &&
+                    <Container>
+                    <Row><button onClick={() => { this.setState({ displayMenu: false }); this.setState({ displayDexList: true }); }}>Dex List</button></Row>
+                    <Row><button onClick={() => { this.setState({ displayMenu: false }); this.setState({ displayItems: true }); }}>Items</button></Row>
+                    </Container>
+                }
+                {
+                    this.state.displayDexList &&
+                    <Container>
+                        {this.state.pokemonData.map(pokemon =>
+                            <button id={pokemon.name} onClick={(e) =>
+                            {
+                                this.getPokemon(e.currentTarget.id);
+                            }}>
+
+                            <Card>
+                                <Card.Img variant="top" src={pokemon.sprites.front_default} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {pokemon.name}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            </button>
+                        )}
+                    </Container>
+                }
+                {
+                    this.state.displayItems &&
+                    <Container>
+                        {this.state.itemData.map(item =>
+                            <button id={item.name} onClick={(e) => {
+                                this.getItem(e.currentTarget.id);
+                            }}>
+                            <Card>
+                                <Card.Img variant="top" src={item.sprites.default} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {item.name}
+                                    </Card.Text>
+                                </Card.Body>
+                                </Card>
+                                </button>
+                        )}
+                    </Container>
+                }
+                {
+                    this.state.displayPokemon &&
+                    <Container>
+                        {
+                            <Card>
+                                <Card.Img variant="top" src={this.state.pokemon.sprites.front_default} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {this.state.pokemon.name}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        }
+                    </Container>
+                }
+                {
+                    this.state.displayItem &&
+                    <Container>
+                        {
+                            <Card>
+                                <Card.Img variant="top" src={this.state.item.sprites.default} />
+                                <Card.Body>
+                                    <Card.Text>
+                                        {this.state.item.name}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        }
+                    </Container>
+                }
             </div>
         );
     }
