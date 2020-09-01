019e252e9e0a7f4538f63c1e5407e54ac514d5f8
Carl <carl.chester.lloyd@gmail.com>
2020-09-01 19:41:38 -0400

    remove useless constructor

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import DexList from './DexList';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-
   render() {
     return (
       <div className="App">
