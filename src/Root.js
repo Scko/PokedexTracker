becec824eb36b53e4bb4849a329b3fd796bc52ff
Carl <carl.chester.lloyd@gmail.com>
2020-09-03 09:52:57 -0400

    navbar

--- src/Root.js
+++ src/Root.js
@@ -7,32 +7,37 @@ import Pokemon from "./Pokemon";
 import './Root.css';
 import logo from './pokeball.svg';
 
+class Root extends React.Component {
+    myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
 
-const Root = () => {
-    return (
-        <div>
+    render() {
+        return (
             <div>
-                <header className="App-header" style={{ display: `flow-root` }}>
-                    <div className="center">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <div>
-                            <h2 className="App-title"> <Link to="/dexlist">DexList</Link></h2>
-                            <h2 className="App-title"> <Link to="/about">About</Link></h2>
+                <header className="App-header">
+                    <div className="topnav" id="myTopnav" >
+                        <div className="center">
+                            <img src={logo} className="App-logo" alt="logo" />
+                            <Link to="/dexlist">DexList</Link>
+                            <Link to="/about">About</Link>
+                            <a href="javascript:void(0);" className="icon" onClick={() => { this.myFunction(); }}>&#9776;</a>
                         </div>
-                    </div>
-
-                    
+                </div>
                 </header>
+                <Switch>
+                    <Route component={About} path="/about" />
+                    <Route component={DexList} path="/dexlist" exact />
+                    <Route component={DexList} path="/" exact />
+                    <Route component={Pokemon} path='/dexlist/:pokemonName' />
+                </Switch>
             </div>
-            
-            <Switch>
-                <Route component={About} path="/about" />
-                <Route component={DexList} path="/dexlist" exact />
-                <Route component={DexList} path="/" exact />
-                <Route component={Pokemon} path='/dexlist/:pokemonName' />
-            </Switch>
-        </div>
-    );
-};
-
+        );
+    };
+}
 export default Root;
